ed81f8cb5ba762e79fecdf0bcdc6bc02740ca7f6
e3psilon <chaou>
2021-09-10 17:56:50 +0200

    init project

---
+++ src/main/java/com/reactive/transaction/backend/TransactionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.transaction.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reactive/transaction/backend/TransactionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.transaction.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
